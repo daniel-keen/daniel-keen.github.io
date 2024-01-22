aae831f919cf6f03b1eee6bbaa32307ab6f9474b
Daniel Keen <denieln@gmail.com>
2024-01-22 20:52:40 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "danielkeen.xyz" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/daniel-keen/" : "/",
   };
   
